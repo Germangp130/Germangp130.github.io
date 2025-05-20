56af2db77ec0a3888d14cd97faab67c5a36487c1
Germangp130 <germanchiguil1@gmail.com>
2025-05-19 20:33:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,7 @@ const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  document.getElementById('nav-links').classList.toggle('show');
+});
+
